e925c944491103e24e07906e7e5c4e3aef03eceb
Kruglyanski <eviltroll@yandex.ru>
2023-06-16 13:38:28 +0300

    some wss

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -16,12 +16,19 @@ export class AppGateway
 {
   private logger: Logger = new Logger('AppGateway');
 
+  @WebSocketServer()
+  private server: Server;
+
   afterInit(server: Server) {
     this.logger.log('AppGateway INIT.');
   }
 
   handleConnection(client: Socket, ...args: any[]) {
+    client.emit('clientConnected', {data: 'DATA CONNECTED'});
     this.logger.log('handleConnection');
+    client.on('disconnecting', async () => {
+        console.log("handleConnection client.on disconnecting")
+    })
   }
 
   handleDisconnect(client: Socket) {
@@ -29,9 +36,21 @@ export class AppGateway
   }
 
   @SubscribeMessage('message')
-  handleMessage(client: Socket, text: string): WsResponse<string> {
-    client.broadcast.emit('message', text);
-
-    return { event: 'message', data: text };
+  handleMessage(client: Socket, incomingData: any): WsResponse<string> {
+    if(incomingData.action === 'chatMessage'){
+client.broadcast.emit('chatMessage', incomingData.data);
+    
+    return { event: 'chatMessage', data:incomingData.data };
+    } else if(incomingData.action === 'anotherMessage'){
+        client.broadcast.emit('anotherMessage', incomingData.data);
+        return { event: 'anotherMessage', data: incomingData.data };
+    }
+     
   }
+
+//   @SubscribeMessage('anotherMessage')
+//   handleAnotherMessage(client: Socket, data: any): WsResponse<any> {
+//      client.broadcast.emit('anotherMessage', data);
+//     return { event: 'anotherMessage', data };
+//   }
 }
